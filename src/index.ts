ee53167d03fa0abc74be8631800f5fea8dd2266c
Alexander Behrens <alexander.behrens@speakap.nl>
2023-01-01 14:22:43 +0100

    adding function body

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
-const func = (value: number): number => value * 2;
+import * as DynamoDb from "aws-sdk/clients/dynamodb.js";
 
-export default func;
+// eslint-disable-next-line unicorn/no-null
+let client: DynamoDb.DocumentClient | null = null;
+
+const getDocumentClient = (): DynamoDb.DocumentClient => {
+    if (!client) {
+        client = new DynamoDb.DocumentClient();
+    }
+    return client;
+};
+
+const paginateQuery = async (
+    params: Omit<DynamoDb.DocumentClient.QueryInput, "Limit"> & { Limit: number },
+    tempItems: DynamoDb.DocumentClient.ItemList = [],
+): Promise<DynamoDb.DocumentClient.QueryOutput> => {
+    const response = await getDocumentClient().query(params).promise();
+    const items = [...tempItems, ...(response.Items || [])];
+    return items.length < params.Limit && response.LastEvaluatedKey
+        ? paginateQuery(
+              {
+                  ...params,
+                  ExclusiveStartKey: response.LastEvaluatedKey,
+              },
+              items,
+          )
+        : {
+              ...response,
+              Items: items,
+          };
+};
+
+const query = async ({
+    queryType,
+    Limit,
+    ...params
+}: DynamoDb.DocumentClient.QueryInput & {
+    queryType?: "stream" | "paginate";
+}): Promise<DynamoDb.DocumentClient.QueryOutput> => {
+    const client = getDocumentClient();
+    if (Limit) {
+        return paginateQuery({ Limit, ...params });
+    } else {
+        return client.query(params).promise();
+    }
+};
+
+export default query;
